11c73d0f4474a4cade1cc9a30b61867b97a02835
unknown <rithwiksingh007@gmail.com>
2021-10-02 23:10:11 +0530

    demo file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import data from "./data.json";
+//components
+import Header from "./Header";
+import ToDoList from "./ToDoList";
+ 
 import './App.css';
-
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [ toDoList, setToDoList ] = useState(data);
+ 
+ 
+ return (
+   <div className="App">
+     <Header />
+     <ToDoList toDoList={toDoList}/>
+   </div>
+ );
 }
-
+ 
 export default App;
---
+++ src/ToDoList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ToDo from './ToDo';
+ 
+ 
+const ToDoList = ({toDoList}) => {
+   return (
+       <div>
+       
+       {toDoList.map(todo => {
+               return (
+                   <ToDo todo={todo} />
+               )
+           })}
+           
+       </div>
+   );
+};
+ 
+export default ToDoList;
